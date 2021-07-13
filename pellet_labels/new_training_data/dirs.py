cab5ab2efe876fdb5b6fb7a7ae3b105f93ec9be0
Jakub Adámek <jakuba@google.com>
2021-07-13 18:19:09 +0100

    Add new pellet labels, add missing methods in dirs.py

--- pellet_labels/new_training_data/dirs.py
+++ pellet_labels/new_training_data/dirs.py
@@ -1,4 +1,21 @@
+import os
+from os import path
+
 # Relative path to read the AST pictures from.
 AST_PICTURES_DIR = "ast_pictures"
 # Relative path to write the cropped pellet images to.
 PELLETS_DIR = "pellets"
+# Relative path to "pellet_labels" directory.
+PELLET_LABELS_DIR = \
+    path.relpath(path.join(path.dirname(path.realpath(__file__)), ".."))
+
+
+def sub_dirs(directory):
+    """Returns the names of all subdirectories."""
+    return [f for f in os.listdir(directory)
+            if path.isdir(path.join(directory, f))]
+
+
+def sub_paths(directory):
+    """Returns the paths to all subdirectories."""
+    return [path.join(directory, f) for f in sub_dirs(directory)]
--- pellet_labels/trainer/pellet_list.py
+++ pellet_labels/trainer/pellet_list.py
@@ -1,12 +1,15 @@
-PELLET_LIST = [
-    'AK 30', 'AM 2', 'AMC 30', 'AMP 10', 'AMP 2', 'ATM 30', 'AUG 30',
-    'AX 20', 'C 30', 'CAZ 10', 'CD 2', 'CFM 5', 'CFR 30', 'CIP 5',
-    'CN 10', 'CN 30', 'CN 500', 'CRO 30', 'CT 50', 'CTX 5', 'E 15',
-    'ERY 15', 'ETP 10', 'FC 10', 'F 100', 'FEC 40', 'FEP 30', 'FF 200',
-    'FOX 30', 'IMI 10', 'IPM 10', 'L 15', 'LEV 5', 'LNZ 10', 'LVX 5',
-    'MEC 10', 'MEM 10', 'MRP 10', 'MXF 5', 'NA 30', 'NET 10', 'NOR 10',
-    'OX 1', 'P 1', 'PRL 30', 'PT 15', 'RA 5', 'RD 5', 'S 300',
-    'SXT 25', 'TC 75', 'TEC 30', 'TEM 30', 'TET 30', 'TGC 15', 'TIC 75',
-    'TIM 85', 'TOB 10', 'TPZ 36', 'TTC 85', 'TZP 36', 'VA 30', 'VA 5']
+PELLET_LIST = ['AK 30', 'AKN 30', 'AM 2', 'AMC 30', 'AMP 10', 'AMP 2', 'API 2',
+               'ATM 30', 'AUG 30', 'AX 20', 'C 30', 'CAZ 10', 'CD 2', 'CFM 5',
+               'CFR 30', 'CHL 30', 'CIP 5', 'CML 30', 'CMN 2', 'CN 10', 'CN 30',
+               'CN 500', 'COX 5', 'CRO 30', 'CT 50', 'CTX 5', 'CZD 10', 'E 15',
+               'ERY 15', 'ETP 10', 'F 100', 'FAD 10', 'FC 10', 'FEC 40',
+               'FEP 30', 'FF 200', 'FOX 30', 'GME 30', 'GMN 10', 'IMI 10',
+               'IPM 10', 'L 15', 'LEV 5', 'LNZ 10', 'LVX 5', 'MEC 10', 'MEM 10',
+               'MRP 10', 'MXF 5', 'NA 30', 'NET 10', 'NOR 10', 'NXN 10', 'OX 1',
+               'OXC 1', 'P 1', 'P 10', 'PEF 5', 'PNG 1', 'PRL 30', 'PT 15',
+               'PTZ 36', 'RA 5', 'RD 5', 'RIF 5', 'S 300', 'SXT 25', 'TC 75',
+               'TCC 85', 'TE 30', 'TEC 30', 'TEM 30', 'TET 30', 'TGC 15',
+               'TIC 75', 'TIM 85', 'TMN 10', 'TOB 10', 'TPZ 36', 'TTC 85',
+               'TZP 36', 'VA 30', 'VA 5', 'VNC 5']
 
 REMOVED_CLASSES = ['AK 30', 'RD 5', 'FEC 40', 'FC 10', 'TGC 15']
